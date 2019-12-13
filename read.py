ed410e17fad3da21313fa3d113b9b18f1197d3f9
李宏君 <homlee@rihiroshikuntekiMacBook-Pro.local>
2019-12-13 14:39:49 +0800

    calculate ave

--- read.py
+++ read.py
@@ -1,10 +1,13 @@
 data = []
 count = 0
+len_reviews_sum = 0
 with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
+		len_reviews_sum = len_reviews_sum + len(line)
 		count += 1
 		if count % 1000 == 0:
 			print(count)
-print(len(data))
-print(data[0])
+
+print('檔案讀取完了,總共有', len(data), '筆留言')
+print('留言平均長度為： ', len_reviews_sum / len(data))
